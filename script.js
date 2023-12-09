1b9b9fe44f782504febffe7dda3e980e4f9938f0
Muhammad Fikri <unicornhorse1@gmail.com>
2023-12-10 00:21:09 +0700

    Adding search film template

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('search-input');
+const btn = document.getElementById('search-btn');
+const main = document.getElementsByTagName('main')[0];
+
+const cardItem = (item) => {
+    return ``
+}
